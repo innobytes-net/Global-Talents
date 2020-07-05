4d74969b499902778d297e933844b0d4673d12c6
cyril1999 <johnsonsachin95@gmail.com>
2020-07-06 01:25:43 +0530

    Registraion with redirect to Multi step

--- signup/registration.php
+++ signup/registration.php
@@ -1,16 +1,16 @@
 <?php
 session_start();
-// $server='162.215.253.205';
-//  $user='innoszdh_global';
-//  $pass='kuber123';
-//  $db='innoszdh_globaltalents';
-//  $port = '3306';
+ $server='162.215.253.205';
+  $user='innoszdh_global';
+  $pass='kuber123';
+  $db='innoszdh_globaltalents';
+  $port = '3306';
 
-$server='localhost';
-$user='root';
-$pass='';
-$db='user';
-$port = '3306';
+// $server='localhost';
+// $user='root';
+// $pass='';
+// $db='user';
+// $port = '3306';
 
 $connection= new mysqli($server,$user,$pass,$db,$port);
 if($connection->connect_error){
